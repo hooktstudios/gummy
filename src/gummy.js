dcea6fd5086b16500f978a9d93eea4541545b576
Léo Renaud-Allaire <leo.renaud@hooktstudios.com>
2013-10-19 14:11:00 -0400

    Improve scrollbars display

--- src/gummy.js
+++ src/gummy.js
@@ -14,15 +14,13 @@ $(function(){
     init: function() {
       this.tableWidth = this.$table.outerWidth();
       this.wrapHeight = this.$wrap.outerHeight();
+      this.theadHeight = this.$thead.outerHeight();
       this.initWrap();
       this.initGummyHead();
     },
     initWrap: function() {
       this.$table.wrap('<div class="gummy-inner-wrap"></div>');
       this.$innerWrap = this.$table.parent();
-      this.$innerWrap.css({
-        height: this.wrapHeight
-      })
     },
     initGummyHead: function() {
       var topOffset = this.$thead.offset().top;
@@ -35,6 +33,15 @@ $(function(){
       // })
 
       this.$wrap.append(this.$stickyHead);
+
+      this.$innerWrap.css({
+        height: this.wrapHeight - this.theadHeight,
+        marginTop: this.theadHeight
+      })
+
+      this.$table.css({
+        marginTop: -this.theadHeight
+      })
     }
   }
 
