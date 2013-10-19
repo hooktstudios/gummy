1242df807c6a73351415281db06463b5b0579ecd
Léo Renaud-Allaire <leo.renaud@hooktstudios.com>
2013-10-19 13:57:24 -0400

    Switch to absolute positionning instead of fixed

--- src/gummy.js
+++ src/gummy.js
@@ -5,6 +5,7 @@ $(function(){
     this.$wrap = $wrap;
     this.$table = $wrap.find('.gummy-table');
     this.$thead = this.$table.find('thead');
+    this.$tbody = this.$table.find('tbody');
 
     this.init();
   }
@@ -12,7 +13,16 @@ $(function(){
   Gummy.prototype = {
     init: function() {
       this.tableWidth = this.$table.outerWidth();
+      this.wrapHeight = this.$wrap.outerHeight();
+      this.initWrap();
       this.initGummyHead();
+    },
+    initWrap: function() {
+      this.$table.wrap('<div class="gummy-inner-wrap"></div>');
+      this.$innerWrap = this.$table.parent();
+      this.$innerWrap.css({
+        height: this.wrapHeight
+      })
     },
     initGummyHead: function() {
       var topOffset = this.$thead.offset().top;
@@ -20,10 +30,9 @@ $(function(){
       this.$stickyHead = $('<table class="gummy-head"></table>');
       this.$stickyHead.html(this.$thead.html());
 
-      this.$stickyHead.css({
-        top: topOffset,
-        width: this.tableWidth
-      })
+      // this.$stickyHead.css({
+      //   top: topOffset
+      // })
 
       this.$wrap.append(this.$stickyHead);
     }
