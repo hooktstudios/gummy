803b8f349b6ccbfeda7589eea0023845b306a4b2
Léo Renaud-Allaire <leo.renaud@hooktstudios.com>
2013-10-19 17:14:05 -0400

    Add scrollbar width detection

--- src/gummy.js
+++ src/gummy.js
@@ -6,6 +6,7 @@ $(function(){
     this.$table = $wrap.find('.gummy-table');
     this.$thead = this.$table.find('thead');
     this.$tbody = this.$table.find('tbody');
+    this.$lastHeader = this.$thead.find('th').last();
 
     this.init();
   }
@@ -15,22 +16,28 @@ $(function(){
       this.tableWidth = this.$table.outerWidth();
       this.wrapHeight = this.$wrap.outerHeight();
       this.theadHeight = this.$thead.outerHeight();
+      this.initialPadding = parseInt(this.$lastHeader.css('paddingRight'), 10);
+      this.scrollbarWidth = this.getScrollbarsWidth();
+
       this.initWrap();
-      this.initGummyHead();
+      this.initHead();
     },
     initWrap: function() {
       this.$table.wrap('<div class="gummy-inner-wrap"></div>');
       this.$innerWrap = this.$table.parent();
     },
-    initGummyHead: function() {
+    initHead: function() {
       var topOffset = this.$thead.offset().top;
 
       this.$stickyHead = $('<table class="gummy-head"></table>');
       this.$stickyHead.html(this.$thead.html());
 
-      // this.$stickyHead.css({
-      //   top: topOffset
-      // })
+      if(this.scrollbarWidth > 0) {
+        this.$stickyHead.find('tr').append($('<th class="scrollbarSpacer"></th>'));
+        this.$stickyHead.find('.scrollbarSpacer').css({
+          width: this.scrollbarWidth + 'px'
+        })
+      }
 
       this.$wrap.append(this.$stickyHead);
 
@@ -42,6 +49,20 @@ $(function(){
       this.$table.css({
         marginTop: -this.theadHeight
       })
+
+    },
+    // Scrollbar width detection snippet from David Walsh
+    // http://davidwalsh.name/detect-scrollbar-width
+    getScrollbarsWidth: function() {
+      var scrollDiv = document.createElement("div");
+      scrollDiv.className = "scrollbar-measure";
+      document.body.appendChild(scrollDiv);
+
+      var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+
+      document.body.removeChild(scrollDiv);
+
+      return scrollbarWidth
     }
   }
 
